681a1ee231aecbf30de6903f9027ad4d483bf9e8
geronimo-lisboa <luciano.geronimo.fnord@gmail.com>
2019-05-23 20:15:53 -0300

    Segundo checkpoint em https://developer.apple.com/library/archive/referencelibrary/GettingStarted/DevelopiOSAppsSwift/ConnectTheUIToCode.html#//apple_ref/doc/uid/TP40015214-CH22-SW1

--- TutorialAppleFoodTracker/ViewController.swift
+++ TutorialAppleFoodTracker/ViewController.swift
@@ -9,11 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealName: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    //MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealName.text = "Default text"
+    }
+    
 
 
 }
