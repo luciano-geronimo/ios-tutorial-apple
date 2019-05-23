40715f03ce15b509d4726f20092dfe4a4bd89742
geronimo-lisboa <luciano.geronimo.fnord@gmail.com>
2019-05-23 19:48:39 -0300

    Initial Commit

---
+++ TutorialAppleFoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TutorialAppleFoodTracker
+//
+//  Created by LUCIANO G LISBOA on 23/05/19.
+//  Copyright © 2019 LUCIANO G LISBOA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
