abd142a51f5010beb3d2e7fff8dbea12f76d3e91
geronimo-lisboa <luciano.geronimo.fnord@gmail.com>
2019-05-29 11:16:17 -0300

    Checkpoint - Consigo abrir a imagem a partir da galeria e mostrá-la no UIImageView

--- TutorialAppleFoodTracker/ViewController.swift
+++ TutorialAppleFoodTracker/ViewController.swift
@@ -9,14 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController,
-    UITextFieldDelegate{
+    UITextFieldDelegate,
+    UIImagePickerControllerDelegate,
+    UINavigationControllerDelegate{
     //MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealName: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    //Relativo ao UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    //Relativo ao UIImagePickerControllerDelegate
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+            else{
+                fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        photoImageView.image = selectedImage
+        dismiss(animated: true, completion: nil)
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         nameTextField.delegate = self
+        let tap = UITapGestureRecognizer(target:self,
+                                         action:#selector(ViewController.selectImageFromPhotoLibrary))
+        photoImageView.addGestureRecognizer(tap)
+        photoImageView.isUserInteractionEnabled = true
+    }
+    
+    @objc func selectImageFromPhotoLibrary(){
+        nameTextField.resignFirstResponder()
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
     }
     //MARK: UIViewController delegate
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
