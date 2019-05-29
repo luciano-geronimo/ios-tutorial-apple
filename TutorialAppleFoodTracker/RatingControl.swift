f59125660727281654a18978eabf559175fa8f80
geronimo-lisboa <luciano.geronimo.fnord@gmail.com>
2019-05-29 20:36:37 -0300

    Esqueletos dos botões espaçados

--- TutorialAppleFoodTracker/RatingControl.swift
+++ TutorialAppleFoodTracker/RatingControl.swift
@@ -1,6 +1,9 @@
 import UIKit
 
 class RatingControl: UIStackView {
+    private var ratingButtons = [UIButton]()
+    var rating = 0
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupButtons()
@@ -11,20 +14,31 @@ class RatingControl: UIStackView {
         setupButtons()
     }
     
-    private func setupButtons(){
-        let button = UIButton()
-        button.backgroundColor = UIColor.red
-        addSubview(button)
-        
+    @objc func ratingButtonTapped(button:UIButton){
+        print("Button pressed 👍")
+    }
+    
+    private func setButtonConstraint(button:UIButton, thingToBeAtLeft:UIView? = nil){
         button.translatesAutoresizingMaskIntoConstraints = false
-        let horizontalConstraint = NSLayoutConstraint(item: button,
-                                                      //attribute: NSLayoutConstraint.Attribute.centerX,
-                                                      attribute: NSLayoutConstraint.Attribute.leading,
-                                                      relatedBy: NSLayoutConstraint.Relation.equal,
-                                                      toItem: self,
-                                                      attribute: NSLayoutConstraint.Attribute.leading,
-                                                      multiplier: 1,
-                                                      constant: 0)
+        var horizontalConstraint : NSLayoutConstraint
+        if(thingToBeAtLeft == nil){
+            horizontalConstraint = NSLayoutConstraint(item: button,
+                                    attribute: NSLayoutConstraint.Attribute.leading,
+                                    relatedBy: NSLayoutConstraint.Relation.equal,
+                                    toItem: self,
+                                    attribute: NSLayoutConstraint.Attribute.leading,
+                                    multiplier: 1,
+                                    constant: 8)
+        }else{
+            horizontalConstraint = NSLayoutConstraint(item: button,
+                                    attribute: NSLayoutConstraint.Attribute.leading,
+                                    relatedBy: NSLayoutConstraint.Relation.equal,
+                                    toItem: thingToBeAtLeft,
+                                    attribute: NSLayoutConstraint.Attribute.trailing,
+                                    multiplier: 1,
+                                    constant: 8)
+        }
+
         let verticalConstraint = NSLayoutConstraint(item: button,
                                                     attribute: NSLayoutConstraint.Attribute.top,
                                                     relatedBy: NSLayoutConstraint.Relation.equal,
@@ -47,5 +61,25 @@ class RatingControl: UIStackView {
                                                   multiplier: 1,
                                                   constant: 44.0)
         self.addConstraints([horizontalConstraint, verticalConstraint, widthConstraint, heightConstraint])
+        
+    }
+    
+    private func setupButtons(){
+        for _ in 0..<5 {
+            let button = UIButton()
+            button.backgroundColor = UIColor.red
+            addSubview(button)
+            button.addTarget(self,
+                         action: #selector(RatingControl.ratingButtonTapped(button:)),
+                         for: .touchUpInside)
+            ratingButtons.append(button)
+        }
+        setButtonConstraint(button: ratingButtons[0])
+        setButtonConstraint(button: ratingButtons[1], thingToBeAtLeft: ratingButtons[0])
+        setButtonConstraint(button: ratingButtons[2], thingToBeAtLeft: ratingButtons[1])
+        setButtonConstraint(button: ratingButtons[3], thingToBeAtLeft: ratingButtons[2])
+        setButtonConstraint(button: ratingButtons[4], thingToBeAtLeft: ratingButtons[3])
+        
+
     }
 }
